6ce7eea7b696f8c94bac28efe91a34cc8ea84f61
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-02-22 13:06:22 +0900

    レスポンスを取得

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,52 @@
 use dotenvy::dotenv;
+use reqwest::{self, Client, Error};
 use std::env;
+use tokio;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     dotenv().expect(".env not found");
     let api_key = match env::var("GEMINI_API_KEY") {
         Ok(key) => key,
         Err(_) => panic!("api key not found"),
     };
 
-    println!("{api_key}");
+    //println!("{api_key}");
+
+    let prompt = String::from("Explain how AI works");
+
+    match ask_ai(&api_key, &prompt).await {
+        Ok(res) => {
+            println!("{}", res);
+        }
+        Err(_) => {
+            println!("failed to ask ai");
+        }
+    }
+}
+
+async fn ask_ai(api_key: &str, prompt: &str) -> Result<String, Error> {
+    let url = format!(
+        "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={}",
+        api_key
+    );
+
+    let client = Client::new();
+    let body = "{
+        \"contents\": [{
+          \"parts\":[{\"text\": \"Explain how AI works\"}]
+          }]
+         }";
+
+    let response = client
+        .post(&url)
+        .header("Content-Type", "application/json")
+        .body(body)
+        .send()
+        .await?
+        .text()
+        .await?;
+
+    println!("{}", response);
+    Ok(response)
 }
