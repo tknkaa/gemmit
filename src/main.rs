8dbf38f7b9cabf7d88d77bd733061ecb5e6580b3
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-02-25 16:56:36 +0900

    feat: Add AI suggested commit messages with user confirmation

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,14 @@ mod git_diff;
 use ai::ask_ai;
 use config::load_api_key;
 use git_diff::get_git_diff;
+use std::io;
 use std::process::Command;
 use tokio;
 
 #[tokio::main]
 async fn main() {
-    /*     let prompt_diff = match get_git_diff() {
+    println!("Gemini suggested the following commit message.");
+    let prompt_diff = match get_git_diff() {
         Ok(diff) => diff,
         Err(_) => {
             println!("Failed to get git diff");
@@ -24,20 +26,33 @@ async fn main() {
         prompt_diff
     );
 
+    let mut message = String::new();
     match ask_ai(&api_key, &prompt).await {
-        Ok(response) => println!("{response}"),
+        Ok(response) => {
+            message.push_str(&response);
+            println!("{response}")
+        }
         Err(_) => println!("Failed to ask AI"),
-    } */
+    }
 
-    let message = "test";
-    let output = Command::new("git")
-        .args(["commit", "-m", message])
-        .output()
-        .unwrap();
+    println!("Do you want to commit with this message?[Y/n]");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("msg");
+    let input = input.trim().to_lowercase();
+    //println!("{input}");
 
-    if output.status.success() {
-        println!("Commit successful");
+    if input == "y" {
+        let output = Command::new("git")
+            .args(["commit", "-m", &message])
+            .output()
+            .unwrap();
+
+        if output.status.success() {
+            println!("Commit successful");
+        } else {
+            eprintln!("Commit failed: {}", String::from_utf8_lossy(&output.stderr));
+        }
     } else {
-        eprintln!("Commit failed: {}", String::from_utf8_lossy(&output.stderr));
+        println!("Commit canceled.");
     }
 }
