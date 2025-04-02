189fb18091fcad301672b3951a097757ed031dbb
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-04-02 14:26:43 +0900

    Exit if GEMINI_API_KEY is not set

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,12 @@
 use std::env;
+use std::process;
 
 pub fn load_api_key() -> String {
-    env::var("GEMINI_API_KEY").expect("API key not found")
+    match env::var("GEMINI_API_KEY") {
+        Ok(key) => key,
+        Err(_) => {
+            eprintln!("GEMINI_API_KEY not found");
+            process::exit(1);
+        }
+    }
 }
