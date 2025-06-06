ec65df11f79f5b9c4eb11a7bacde8c0ddf90abcd
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-06-06 14:45:28 +0900

    feat: Add option to specify starting word for commit message

--- src/cli.rs
+++ src/cli.rs
@@ -3,6 +3,10 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(version, about = env!("CARGO_PKG_DESCRIPTION"), long_about = None)]
 pub struct Args {
-    #[arg(short, long)]
+    #[arg(
+        short,
+        long,
+        help = "Specify a word for your commit message to start with"
+    )]
     pub start: Option<String>,
 }
