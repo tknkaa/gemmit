71296202f1ada2d74f8aa1ee8335dd594c767a84
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-06-15 11:02:35 +0900

    feat: Add option to include specific words in commit message

--- src/cli.rs
+++ src/cli.rs
@@ -9,4 +9,6 @@ pub struct Args {
         help = "Specify a word for your commit message to start with"
     )]
     pub start: Option<String>,
+    #[arg(short, long, help = "Specify words for your commit message to include")]
+    pub include: Option<Vec<String>>,
 }
--- src/prompt.rs
+++ src/prompt.rs
@@ -7,6 +7,14 @@ pub fn create_prompt(diff: &str, filepath: &str, args: &Args) -> String {
         let additional_prompt = format!("\nstart with {}\n", start);
         template.push_str(&additional_prompt);
     }
+    if let Some(include) = &args.include {
+        let additional_prompt = format!(
+            "\n include the following words properly: {}",
+            include.join(",")
+        );
+        template.push_str(&additional_prompt);
+    }
     template.push_str(diff);
+    print!("{template}");
     template
 }
