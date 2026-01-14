bf0d37ea55cf7cf410d01ae26865e4e86df813a3
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2026-01-14 19:24:14 +0800

    chore: initialize cobra project

--- flake.nix
+++ flake.nix
@@ -1,33 +1,13 @@
 {
-  description = "Gemmit";
+  description = "Simple flake with a devshell";
 
+  # Add all your dependencies here
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+    blueprint.url = "github:numtide/blueprint";
+    blueprint.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    rust-overlay,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      overlays = [(import rust-overlay)];
-      pkgs = import nixpkgs {
-        inherit system overlays;
-      };
-      version = (fromTOML (builtins.readFile ./Cargo.toml)).package.version;
-      toolchain = pkgs.rust-bin.stable.latest.default;
-      package = pkgs.callPackage ./default.nix {inherit version toolchain;};
-    in {
-      formatter = pkgs.alejandra;
-      devShells.default = pkgs.mkShell {
-        packages = [toolchain];
-      };
-      packages.gemmit = package;
-      packages.default = package;
-    });
+  # Load the blueprint
+  outputs = inputs: inputs.blueprint { inherit inputs; };
 }
---
+++ main.go
@@ -0,0 +1,10 @@
+/*
+Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+*/
+package main
+
+import "github.com/tknkaa/gemmit/cmd"
+
+func main() {
+	cmd.Execute()
+}
