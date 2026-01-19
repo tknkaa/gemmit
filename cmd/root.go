7f88f2fb0019f831de9417b78e1f1351ab83557a
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2026-01-19 18:45:04 +0800

    feat: add status message during processing

--- cmd/root.go
+++ cmd/root.go
@@ -26,6 +26,7 @@ to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Thinking...")
 		ctx := context.Background()
 		client, err := genai.NewClient(ctx, nil)
 		if err != nil {
